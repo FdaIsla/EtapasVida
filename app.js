2700e36ab78b1db61ab52a0a9c6c4d1d11b99b80
iamfernanda <fda.isla@gmail.com>
2018-05-28 13:54:30 -0400

    casi final

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
 
 function myFunction() {
     document.getElementById("demo").innerHTML = years;
-    
+    document.getElementById("primeraVentana").style.display="none";
+
     var years = prompt("Ingrese su año de nacimiento");  
     var edad = (2018 - years); 
          
 
-            if (edad===2018){
-                document.write("Usted es un bebezito " );
-            }
+            // if (edad===2018){
+            //     document.write("Usted es un bebezito " );
+            // }
 
-            else if (edad===2|| edad===1) {
-                document.write("Usted es un niño pequeño " );
+            if (edad===2|| edad===1 || edad==3) {
+                // document.write("Usted es un niño pequeño " );
+                document.getElementById('results').innerHTML = '<p> Usted es un niño pequeños </p>';
              }
 
             else if (edad==4 || edad==5) {
-                 document.write("Usted es preescolar" );
+                 document.getElementById('results').innerHTML = '<p> Usted es preescolar </p>';
              }
 
 
             else if (edad==6|| edad==7 || edad==8 || edad==9 || edad==10 || edad==11 || edad==12) {
-                 document.write("Usted es Gradeshooller " );
+                 document.getElementById('results').innerHTML = '<p> Usted es Gradeshooller </p>';
             }
 
             else if (edad==13|| edad==14 ||edad==15 ||edad==16 ||edad==17 || edad==18){
-                 document.write("Usted es adolecente " );
+                 document.getElementById('results').innerHTML = '<p> Usted es adolecente  </p>';
             }
 
             else if (edad==18 || edad==19 ||edad==20 || edad<21){
-                 document.write("Usted es adulto joven " );
+                 document.getElementById('results').innerHTML = '<p> Usted es adulto joven </p>';
              }
 
             else if (edad>22){
-                document.write( "A estas alturas usted ya es Adulto. Ahora comienza lo bueno" );
+                document.getElementById('results').innerHTML = '<p> A estas alturas usted ya es Adulto. Ahora comienza lo bueno </p>';
 
             } else   {
                          alert("Dato erroneo" );
